57fd287b2b1edcaa53d2300591b802423bdc55ec
Jayme Campos <jayme.campos17@gmail.com>
2024-02-09 17:30:57 -0300

    function to active accordion list

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
+//Tab navigation//
 const tabMenu = document.querySelectorAll('.js-tabmenu li');
 const tabContent = document.querySelectorAll('.js-tabcontent section')
 
-if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('active');
+function initNavTab() {
 
-    function activeTab(index) {
-        tabContent.forEach((section) => {
-            section.classList.remove('active');
-        })
-        tabContent[index].classList.add('active');
-    }
-
-    tabMenu.forEach((itemMenu, index) => {
-        itemMenu.addEventListener('click', () => {
-            activeTab(index);
+    if (tabMenu.length && tabContent.length) {
+        tabContent[0].classList.add('active');
+    
+        function activeTab(index) {
+            tabContent.forEach((section) => {
+                section.classList.remove('active');
+            })
+            tabContent[index].classList.add('active');
+        }
+    
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => {
+                activeTab(index);
+            });
         });
-    });
+    };
 }
+
+initNavTab();
+
+//Accordion List//
+const accordion = document.querySelectorAll('.js-accordion dt');
+
+function initList() {
+
+    if (accordion.length) {
+        const active = 'active';
+        accordion[0].classList.add(active);
+        accordion[0].nextElementSibling.classList.add(active);
+    
+        function activeList() {
+            this.classList.toggle(active);
+            this.nextElementSibling.classList.toggle(active);
+        };
+    
+        accordion.forEach((itemFaq) => {
+            itemFaq.addEventListener('click', activeList);
+        });
+    }   
+}
+
+initList();
