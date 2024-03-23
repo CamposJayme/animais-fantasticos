33cbd66ea281325361ecdc2839afffff1768104f
Jayme Campos <jayme.campos17@gmail.com>
2024-03-23 13:42:54 -0300

    soft scroll animation

--- script.js
+++ script.js
@@ -46,3 +46,27 @@ function initList() {
 }
 
 initList();
+
+
+//Scroll suave//
+function ativaScrollSuave() {
+    const linksInternos = document.querySelectorAll('.menu a[href^="#"]');
+
+    if (linksInternos.length) {
+        function scrollSuave(event) {
+            event.preventDefault();
+            const href = event.currentTarget.getAttribute('href');
+            const sections = document.querySelector(href);
+            sections.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start',
+            })
+        }
+        
+        linksInternos.forEach((link) => {
+            link.addEventListener('click', scrollSuave);
+        })
+    }
+}
+
+ativaScrollSuave();
